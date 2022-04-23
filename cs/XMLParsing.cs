c12a9ce92c13db5e76545e885b0c73dc8774f7f0
makeit-mike <mike.isaac.jones@gmail.com>
2022-04-23 11:52:07 -0500

    Adding XML example

---
+++ cs/XMLParsing.cs
@@ -0,0 +1,10 @@
+namespace cs;
+
+public class XMLParsing
+{
+    // If you are told to handle XML, you done made mistakes.
+    public static void ParseXML()
+    {
+        Console.WriteLine("Convert your XML into JSON, YAML, or literally anything else");
+    }
+}
