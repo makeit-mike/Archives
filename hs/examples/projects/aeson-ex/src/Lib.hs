d1d213e107c299aacf1cde7d77f52389d653fb54
makeit-mike <mike.isaac.jones@gmail.com>
2022-04-27 13:44:04 -0500

    Adding test files, playing around with aeson

--- hs/examples/projects/aeson-ex/src/Lib.hs
+++ hs/examples/projects/aeson-ex/src/Lib.hs
@@ -1,31 +1,40 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-import qualified Data.Text.IO as T
-import Data.Aeson
-import Data.Text
-import GHC.Generics
+import           Data.Aeson
+import           Data.Text
+import qualified Data.Text.IO     as T
+import           GHC.Generics
+import           System.Directory
 
-data Person = Person { name :: Text
-                     , age :: Int 
-                     } deriving (Generic, Show)
+data Person =
+  Person
+    { name :: Text
+    , age  :: Int
+    }
+  deriving (Generic, Show)
 
 instance FromJSON Person
+
 instance ToJSON Person
+
+jsonFile1 = readFile "../../"
+
+currDir = getCurrentDirectory
 
 someFunc :: IO ()
 someFunc = do
-    putStrLn $ "Joe's Age: " ++ show (age joe)
-    putStrLn $ "Encode: " ++ show (encode joe)
-    putStrLn $ "Decode: " ++ show l
-    putStrLn $ "Name: " ++ show (maybe "" name l)
-        where l = decode encodedJoe :: Maybe Person
+  putStrLn $ "Joe's Age: " ++ show (age joe)
+  putStrLn $ "Encode: " ++ show (encode joe)
+  putStrLn $ "Decode: " ++ show l
+  putStrLn $ "Name: " ++ show (maybe "" name l)
+  where
+    l = decode encodedJoe :: Maybe Person
 
 helloWorld = T.putStrLn "Hello, World!"
 
-joe = Person { name = "Joe", age = 12 }
+joe = Person {name = "Joe", age = 12}
+
 encodedJoe = "{ \"name\": \"Joe\", \"age\": 12 }"
